ba2e2b92893081bc77f0ac09f3e2e9ea6da3af7a
FabioDaSilva73 <fabiocastro231999@gmail.com>
2021-06-02 09:36:54 -0300

    testing the modules and call of function

---
+++ testeAvisos/avisos.py
@@ -0,0 +1,12 @@
+def atencao():
+	#teste
+	print("\nQuer acessar esta merda antes de estudar FDP\n");
+
+def verificarSeAFrasesContemA():
+	print("Digite uma frase:");
+	nome = input();
+	if 'a' in nome:
+		print("\nYeah, the caracter 'A' is into the pharse");
+
+	else:
+		print("\nNo, the caracter 'A' isn't into the pharse");
